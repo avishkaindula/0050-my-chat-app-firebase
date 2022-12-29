cc46ca91a62f7226d87b1b77a3c93f1fc10ee60c
Avishka Indula <avishkainduladev@gmail.com>
2022-12-29 07:36:22 +0530

    Creating Chat.js file and adding ChatEngine
    => creating Chats.js file
    => Now even if we manually navigate to "/" home page
    *  it will redirect us back to "/chats" page.
    => creating the jsx code in Chat.js which is needed to crete the chats page.
    => going to chatengine.io , create an account and get the projectId
    => adding projectId as a prop to <ChatEngine />
    => adding a onClick listener to Logout button and create handleLogout function.

---
+++ src/components/Chats.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
+
+const Chats = () => {
+  const history = useHistory();
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push("/");
+    // This will redirect us back to the "/" home page (Login page)
+    // once we signed out.
+  };
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Unichat</div>
+
+        <div onClick={handleLogout} className="logout-tab">
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectID="47e205cb-ec9f-4bfc-9e04-caa92f2e3904"
+        userName="."
+        userSecret="."
+        // We need to be able to grab the userName and the userSecret from firebase auth
+        // to be able to logged in
+      />
+    </div>
+  );
+};
+
+export default Chats;
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -12,15 +12,18 @@ export function useAuth() {
 
 export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
       setLoading(false);
-      history.push("/chats");
-      // history.push will re-navigate us to our chat application.
+      if (user) history.push("/chats");
+      // history.push will re-navigate us to our /chats page.
+      // if (user) will make user that we only navigate to /chats if we have a user.
+      // I think it also means that if we have logged in as a user and try to navigate
+      // to the "/" page (Login page), we will automatically get redirected to "/chats" page.
     });
   }, [user, history]);
   // We need to call useEffect whenever we add a user or when we re-navigate.
