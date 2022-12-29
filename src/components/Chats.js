7f54ba9f0f54de7e5b21a4386afefdab30709650
Avishka Indula <avishkainduladev@gmail.com>
2022-12-29 12:46:53 +0530

    Creating chat users and finalize the project
    => now we have to call the ChatEngine api to create chat users
    *  by using the auth data profided by firebase authentication.
    => import { useRef, useState, useEffect }
    => creating useEffect(() => {}) function.
    => axios.get("https://api.chatengine.io/users/me/")
    *  this will get chat data from an already existing user.
    => axios.post("https://api.chatengine.io/users")
    => remove the  getFile(user.photoURL) function because it
    *  throws an error.
    => Creating Environment variables in .env and add them to Chat.js
    => after git committing all files, we can finally run
    *  npm run build
    => drag and drop build folder to netlify
    => now we need to add the netlify url to authorized domains in
    *  authentication field of firebase.
    => we also might need to add that url to the developers.facebook.commit
    *  settings.

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
+import axios from "axios";
 
 import { useAuth } from "../contexts/AuthContext";
 
@@ -13,7 +14,9 @@ const Chats = () => {
   // to AuthContextProvider through value prop in AuthContext.js
   // <AuthContext.Provider value={value}>
   // {value} holds the user data
-  console.log(user);
+  // console.log(user);
+
+  const [loading, setLoading] = useState(true);
 
   const handleLogout = async () => {
     await auth.signOut();
@@ -21,6 +24,70 @@ const Chats = () => {
     // This will redirect us back to the "/" home page (Login page)
     // once we signed out.
   };
+
+  // ====================================================================================
+  // const getFile = async (url) => {
+  //   const response = await fetch(url);
+  //   const data = await response.blob();
+  //   // blob() will allow us to transfer files in binary format like images or any
+  //   // other type of file. This blob contains our image.
+  // =================THIS THROWS AN ERROR SO I HAD TO REMOVE  IT========================
+  // ====================================================================================
+
+  //   return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  // };
+  // This function will allow users to have an image
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    // This will re-direct to the homepage if no user is found.
+
+    axios
+      .get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+          "user-name": user.email,
+          "user-secret": user.uid,
+          // This user-name and user-secret data is coming from useAuth context
+        },
+      })
+      .then(() => {
+        setLoading(false);
+        // This will make a call to chatengine.io/users/me by using the headers provided.
+        // This will load the chat data from an "already created user."
+      })
+      .catch(() => {
+        let formdata = new FormData();
+        formdata.append("email", user.email);
+        formdata.append("username", user.email);
+        formdata.append("secret", user.uid);
+        // This will create a new user if there's no user found in the ChatEngine
+
+        // ====================================================================================
+        // getFile(user.photoURL)
+        //   .then((avatar) => {
+        //     formdata.append("avatar", avatar, avatar.name);
+        // });
+        // =================THIS THROWS AN ERROR SO I HAD TO REMOVE  IT========================
+        // ====================================================================================
+
+        axios
+          .post("https://api.chatengine.io/users", formdata, {
+            headers: {
+              "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY,
+            },
+          })
+          .then(() => setLoading(false))
+          .catch((error) => console.log(error));
+        // This will finally post a new user in chatengine
+      });
+  }, [user, history]);
+
+  if (!user || loading) return "Loading...";
+  // This will return a string called "Loading..." if there's no user or the app is still loading
 
   return (
     <div className="chats-page">
@@ -34,9 +101,9 @@ const Chats = () => {
 
       <ChatEngine
         height="calc(100vh - 66px)"
-        projectID="47e205cb-ec9f-4bfc-9e04-caa92f2e3904"
-        userName="."
-        userSecret="."
+        projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName={user.email}
+        userSecret={user.uid}
         // We need to be able to grab the userName and the userSecret from firebase auth
         // to be able to logged in
       />
